85ce8b9671656e0fd1188e9cf72f6f63471386a3
Anthony Ford <ford.anthonyj@gmail.com>
2012-01-02 01:18:39 -0600

    manage.py changes.
    Added categories command. Basic functionality for now.
    Also rolled password checking into function.

--- RGVRSEF/config.py
+++ RGVRSEF/config.py
@@ -1,3 +1,21 @@
 SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
 SECRET_KEY = 'developmentKey'
-
+CATEGORIES = (  ("Team Physical",1),
+                ("Team Biological",1),
+                ("Animal Science",0),
+                ("Behavioral and Social Science",0),
+                ("Biochemistry",0),
+                ("Cellular and Molecular Biology",0),
+                ("Chemistry",0),
+                ("Computer Science",0),
+                ("Earth and Planetary Science",0),
+                ("Engineering: Electrical and Mechanical",0),
+                ("Engineering: Materials and Bioengineering",0),
+                ("Energy and Transportation",0),
+                ("Environmental Management",0),
+                ("Environmental Sciences",0),
+                ("Mathematical Sciences",0),
+                ("Medicine and Health Sciences",0),
+                ("Microbiology",0),
+                ("Physics &amp; Astronomy",0),
+                ("Plant Sciences",0))
--- manage.py
+++ manage.py
@@ -12,29 +12,48 @@ import RGVRSEF.admin.models as admin_models
 manager = Manager(app)
 manager.add_command("runserver", Server())
 
+def password_valid():
+    passwd = sha256(prompt_pass("Please enter admin password")).hexdigest()
+    if passwd == admin_models.Admin.query.filter_by(username='root').\
+                    first().password:
+        return True
+    else:
+        print "Invalid Password"
+        return False
+
+
+# Build Test DB
 @manager.command
 def testdb():
-    try:
-        import test_rgvrsef
-    except ImportError:
-        print "Unable to load module 'test_rgvrsef' required to build test db"
-        return 1
-    test_rgvrsef.build_and_populate()
-    print "Test database created and populated"
+    """ Management function to create test DB. Requires root admin access. """
+    if prompt_bool("Are you sure you want to loose all data?"):
+        if password_valid():
+            try:
+                import test_rgvrsef
+            except ImportError:
+                print "Unable to load module 'test_rgvrsef' \
+                      required to build test db"
+                return 1
+            test_rgvrsef.build_and_populate()
+            print "Test database created and populated"
+        else:
+            print 'Database operation aborted.'
 
+# Drop DB
 @manager.command
 def dropdb():
+    """ Management function to drob DB. Requires root admin access. """
     if prompt_bool("Are you sure you want to loose all data?"):
-        passwd = sha256(prompt_pass("Please enter admin password")).hexdigest()
-        if passwd == admin_models.Admin.query.filter_by(username='root').\
-                        first().password:
+        if password_valid(): 
             models.db.drop_all()
             print 'Database dropped'
         else:
-            print 'Invalid Password. Database operation aborted.'
+            print 'Database operation aborted.'
 
+# Build DB
 @manager.command
 def builddb():
+    """ Management function to create the required DB. """ 
     models.db.create_all()
     print 'Blank database created'
     passwd = None
@@ -47,24 +66,37 @@ def builddb():
     models.db.session.add(admin_models.Admin('root',passwd,True))
     models.db.session.commit()
 
+# Add Admin
 @manager.command
 def admin():
-    rootpasswd = sha256(prompt_pass("Please enter admin password")).hexdigest()
-    if rootpasswd == admin_models.Admin.query.filter_by(username='root').\
-                    first().password:
+    """ Management function to add admins. """
+    if password_valid(): 
         username = None
         while username is None:
             username = prompt("Please enter new admin username")
         passwd = None
         while passwd is None:
-            passwd = prompt_pass("Please enter new admin password")
+            passwd = prompt_pass("Please enter password for user '%s'"%username)
             passwd_confirm = prompt_pass("Please confirm password")
             if passwd != passwd_confirm:
                 print "Passwords do not match."
                 passwd = None
         models.db.session.add(admin_models.Admin(username,passwd,True))
         models.db.session.commit()
+    else:
+        print "Admin creation aborted"
 
+@manager.command
+def categories():
+    """ Management function to import categories. """ 
+    if password_valid():
+        for category in app.config['CATEGORIES']:
+            cat = models.Category(category[0],category[1])
+            models.db.session.add(cat)
+        models.db.session.commit()
+        print "Categories successully added."
+    else:
+        print "Category import aborted."
 
 
 if __name__ == '__main__':
