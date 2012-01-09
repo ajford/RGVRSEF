8a8b2d8daf2e11c2eca2b597ce1dba4ab49947ef
Anthony Ford <ford.anthonyj@gmail.com>
2012-01-09 14:07:36 -0600

    Wrong size for password field

--- RGVRSEF/admin/models.py
+++ RGVRSEF/admin/models.py
@@ -7,7 +7,7 @@ from RGVRSEF import db
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
-    password = db.Column(db.String(50))
+    password = db.Column(db.String(70))
     active = db.Column(db.Boolean, default=False)
 
     def __init__(self,username,password,active=False):
