b230b7e0578ff1d20cb4f3a9f113854c16a32d19
Anthony Ford <ford.anthonyj@gmail.com>
2012-02-25 11:17:43 -0600

    Adding another test view

--- RGVRSEF/testing/views.py
+++ RGVRSEF/testing/views.py
@@ -3,6 +3,7 @@ from pprint import pformat
 from flask import redirect, render_template, url_for, make_response,session
 
 from .. import app
+from .. import models
 from .. import utils
 
 @app.route('/testing/session')
@@ -13,3 +14,11 @@ def test_session_view():
 def test_session_clear():
     utils.clear()
     return redirect(url_for('test_session_view'))
+
+
+@app.route('/testing/projectreview/<int:id>',methods=['GET'])
+def test_project_review(id):
+    project = models.Project.query.get_or_404(id)
+    
+    leader = project.student.filter(models.Student.team_leader==True).first()
+    return render_template('complete.html', leader=leader)
